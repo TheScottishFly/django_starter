0cc0c823753cca90b737e58360bfde50338e95be
Gilian Rosnet boutinet <grosnet-@z3r1p2.le-101.fr>
2018-09-09 20:49:53 +0200

    init

---
+++ django_start.py
@@ -0,0 +1,20 @@
+#!/Users/grosnet-/.brew/bin/python3
+
+import os, argparse
+
+def make_app(name):
+    os.system("pip3 install -r requirements.txt")
+    os.system("python3 -m django startproject {}".format(name))
+    os.mkdir("{}/apps".format(name))
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("app_name", help="Application name")
+    args = parser.parse_args()
+    if len(args.app_name) > 0:
+        make_app(args.app_name)
+    else:
+        parser.print_help()
+
+if __name__ == '__main__':
+    main()
