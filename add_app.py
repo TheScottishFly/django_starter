00ce112499e978ecb81c69c52b0524607fe06780
Gilian Rosnet boutinet <grosnet-@z4r1p5.le-101.fr>
2018-09-17 09:40:49 +0200

    continue add app

--- add_app.py
+++ add_app.py
@@ -1,12 +1,18 @@
 from django.core.management.base import BaseCommand
 from django.conf import settings
+import os
 
 class Command(BaseCommand):
     args = '<app_name>'
 
     def handle(self, *args, **kwargs):
-        with open("{}/{}/additionals.py".format(settings.BASE_DIR, settings.BASE_DIR.split("/")[-1]), "w+") as f:
+        base = settings.BASE_DIR
+        proj = settings.BASE_DIR.split("/")[-1]
+        name = args[0]
+        with open("{}/{}/additionals.py".format(base, proj), "w+") as f:
             for line in f.readlines():
                 if ']' in line:
-                    f.write("    {}\n".format(args[0]))
+                    f.write("    {}\n".format(name))
                 f.write(line)
+        os.mkdir("{}/apps/{}".format(base, name))
+        os.system("python3 -m django startapp {} {}/apps/{}".format(name, base, name))
