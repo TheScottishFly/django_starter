099dd2a4d6c0a7636de2150338707dbb177c35ff
Gilian Rosnet boutinet <grosnet-@z4r1p5.le-101.fr>
2018-09-29 12:14:42 +0200

    fix mv

--- add_app.py
+++ add_app.py
@@ -16,19 +16,20 @@ class Command(BaseCommand):
             name = kwargs['app_name'][0]
         except:
             self.stdout.write("WARNING !!")
-        os.mkdir("{}/apps/{}".format(base, name))
-        os.system("python3 -m django startapp {} {}/apps/{}".format(name, base, name))
-        with open("{}/apps/{}/urls.py".format(base, name), "a+") as f:
-            f.write("from django.urls import path\n\nurlpatterns = [\n\n]\n")
-        with open("{}/apps/{}/forms.py".format(base, name), "a+") as f:
-            f.write("from django import forms\n\n")
-        self.stdout.write("{} added !".format(name))
-        with open("{}/{}/additionals.py".format(base, proj), "a+") as f:
-            f.seek(0)
-            input = "".join(f.readlines())
-            input = input[11:]
-            input = json.loads(input)
-            input.append(name)
-        with open("{}/{}/additionals.py".format(base, proj), "w+") as f:
-            f.write("ADD_APPS = ")
-            json.dump(input, f)
+        else:
+            os.system("python3 {}/manage.py startapp {}".format(base, name))
+            os.system("mv {}/{} {}/apps/".format(base, name, base))
+            with open("{}/apps/{}/urls.py".format(base, name), "a+") as f:
+                f.write("from django.urls import path\n\nurlpatterns = [\n\n]\n")
+            with open("{}/apps/{}/forms.py".format(base, name), "a+") as f:
+                f.write("from django import forms\n\n")
+            self.stdout.write("{} added !".format(name))
+            with open("{}/{}/additionals.py".format(base, proj), "a+") as f:
+                f.seek(0)
+                input = "".join(f.readlines())
+                input = input[11:]
+                input = json.loads(input)
+                input.append(name)
+            with open("{}/{}/additionals.py".format(base, proj), "w+") as f:
+                f.write("ADD_APPS = ")
+                json.dump(input, f)
